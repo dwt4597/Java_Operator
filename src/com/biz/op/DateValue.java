e46157ae087ae2a8e1a1d8428b0bc93be4feeb0e
ijs9200 <ijs9200@naver.com>
2018-11-27 21:45:18 +0900

    first

---
+++ src/com/biz/op/DateValue.java
@@ -0,0 +1,36 @@
+package com.biz.op;
+
+import java.text.*;
+import java.util.*;
+
+public class DateValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 날짜와 관련돤 JDK 이슈
+		// JAVA 1.7이하 호환
+		Date date = new Date();
+		System.out.println(date.toString());
+		System.out.println(date.getDate()); // 쓰지마라
+		System.out.println(date.getTime());
+
+		SimpleDateFormat dateForm = new SimpleDateFormat("yyyy년 MM월 dd일");
+		SimpleDateFormat timeForm = new SimpleDateFormat("hh:mm:ss");
+
+		String nowDate = dateForm.format(date);
+		String nowTime = timeForm.format(date);
+
+		System.out.println("오늘 날짜 : " + nowDate);
+		System.out.println("현재 시각 : " + nowTime);
+
+		Calendar cal = Calendar.getInstance();
+		// nowDate = dateForm.format(cal.getDate());
+		nowTime = timeForm.format(cal.getTime());
+
+		System.out.println("오늘 날짜 : " + nowDate);
+		System.out.println("현재 시각 : " + nowTime);
+
+	}
+
+}
